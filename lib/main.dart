9706849dd425f602a9e4a900973a80c326114d9f
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-01-19 20:11:22 +0630

    Flat Design

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,40 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         centerTitle: true,
         title: Text('Flutter modern 3D design'),
       ),
       body: Center(
-        child: Icon(
-          Icons.android,
-          size: 60,
-          color: Colors.grey[800],
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[600],
+                // shadow direction (vertical axis, horizontal axis)
+                offset: Offset(4.0, 4.0),
+                // make blur effect to elevation
+                blurRadius: 15.0,
+                spreadRadius: 1.0,
+              ),
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(-4.0, -4.0),
+                blurRadius: 15.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          child: Icon(
+            Icons.android,
+            size: 60,
+            color: Colors.grey[800],
+          ),
         ),
       ),
     );
